4a30bb321a40055e9f8988d4b235697bbede8f2c
Kamranali9713 <kamranali.9713@gmail.com>
2025-12-23 16:36:24 +0500

    user api created

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const routes = require("./routes");
+const userRouter = require("./routes/userRoutes")
+
 
 const app = express();
 
 app.use(express.json());
 app.use("/", routes);
+app.use("/api/user", userRouter)
 
 module.exports = app;
